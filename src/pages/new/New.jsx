3b8142e866ebc377e8429e6b12b5b5c892f0f062
PattyCutie <iamsureiamcute@gmail.com>
2022-08-03 11:01:14 +0200

    Add handle input submit and firebase addDoc-collection function

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -4,10 +4,35 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadRoundedIcon from "@mui/icons-material/DriveFolderUploadRounded";
 
-function New({inputs, title}) {
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+import { db } from "../../firebase";
 
-  const [file, setFile] = useState("")
+function New({ inputs, title }) {
+  const [file, setFile] = useState("");
   //console.log(file)
+
+  // Add a new document in collection "<*collection name*>"
+  const handleAdd = async (e) => {
+    e.preventDefault();
+    try {
+      // add document in collection
+      const res = await addDoc(collection(db, "cities"), {
+        name: "Los Angeles",
+        state: "CA",
+        country: "USA",
+        timeStamp: serverTimestamp(),
+      });
+    } catch (err) {
+      // catch err
+      console.log();
+    }
+  };
 
   return (
     <div className="new">
@@ -24,32 +49,33 @@ function New({inputs, title}) {
             <img
               src={
                 file
-                ? URL.createObjectURL(file)
-                : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
               }
               alt=""
             />
           </div>
           <div className="right">
-            <form>
+            <form onSubmit={handleAdd}>
               <div className="formInput">
                 <label htmlFor="file">
-                 Image: <DriveFolderUploadRoundedIcon className="icon"/>
+                  Image: <DriveFolderUploadRoundedIcon className="icon" />
                 </label>
-                <input 
-                type="file" 
-                id="file"
-                onChange={(e) => setFile(e.target.files[0])} 
-                style={{ display: "none" }}/>
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: "none" }}
+                />
               </div>
-              
-              {inputs.map((input) =>( 
-              <div className="formInput" key={input.id}>
-                <label>{input.label}</label>
-                <input type={input.type} placeholder={input.placeholder} />
-              </div>
-             ))}
-              <button>Send</button>
+
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
+              <button type="submit">Send</button>
             </form>
           </div>
         </div>
